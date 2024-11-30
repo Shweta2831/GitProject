92ec731ad7f4ceacc69ce25065e8a585b2b8b010
Shweta2831 <shweta.vs2831@gmail.com>
2024-11-30 23:58:01 +0530

    demo2 file created

--- src/sampleGit/demo2.java
+++ src/sampleGit/demo2.java
@@ -1,12 +1,11 @@
 package sampleGit;
 
-public class Sample {
+public class demo2 {
 
 	public static void main(String[] args) {
 
 
-		System.out.println("sample1 running");
-		
+		System.out.println("Running demo2 class");
 	}
 
 }
